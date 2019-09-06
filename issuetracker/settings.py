9f7168a041a201b4cbd9480149ac6d2d384c0097
Raj <r.bhadauria7@gmail.com>
2019-09-06 22:42:22 +0100

    local postgres database added

--- issuetracker/settings.py
+++ issuetracker/settings.py
@@ -73,10 +73,22 @@ WSGI_APPLICATION = 'issuetracker.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'issuetracker',
+        'USER':'postgres',
+        'PASSWORD':'YumiUK100',
+        'HOST':'localhost',
+        'PORT':'5432',
     }
 }
 
