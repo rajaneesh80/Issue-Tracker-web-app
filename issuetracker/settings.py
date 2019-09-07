403033804fb5363af88551bb3cb14d1348612832
Raj <r.bhadauria7@gmail.com>
2019-09-07 01:13:39 +0100

    custome css added

--- issuetracker/settings.py
+++ issuetracker/settings.py
@@ -133,6 +133,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), )
 
 STRIPE_PUBLISHABLE = os.getenv('STRIPE_PUBLISHABLE')
 STRIPE_SECRET = os.getenv('STRIPE_SECRET')
